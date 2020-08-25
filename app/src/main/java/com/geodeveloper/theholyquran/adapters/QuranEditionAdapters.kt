8a4efaa99612abd5989aafc441a91c835d021396
EjilolaHammedEjitomiwa <geotechbrain@gmail.com>
2020-08-25 07:21:01 -0700

    Added quran edition details

--- app/src/main/java/com/geodeveloper/theholyquran/adapters/QuranEditionAdapters.kt
+++ app/src/main/java/com/geodeveloper/theholyquran/adapters/QuranEditionAdapters.kt
@@ -1,5 +1,6 @@
 package com.geodeveloper.theholyquran.adapters
 
+import android.app.AlertDialog
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
@@ -10,24 +11,37 @@ import androidx.annotation.NonNull
 import androidx.recyclerview.widget.RecyclerView
 import com.geodeveloper.theholyquran.R
 import com.geodeveloper.theholyquran.models.QuranEditionModel
+import kotlinx.android.synthetic.main.d_quran_edition_details.*
 
-class QuranEditionAdapters(val context: Context, val editionLists:QuranEditionModel):
-    RecyclerView.Adapter<QuranEditionAdapters.ViewHolder?>() {
+class QuranEditionAdapters(val context: Context, val editionLists: QuranEditionModel) :
+        RecyclerView.Adapter<QuranEditionAdapters.ViewHolder?>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):ViewHolder {
-        val view = LayoutInflater.from(context).inflate(R.layout.d_quran_edition_design,parent,false)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.d_quran_edition_design, parent, false)
         return ViewHolder(view)
     }
 
     override fun getItemCount(): Int {
         return editionLists.results!!.size
     }
-    override fun onBindViewHolder(holder:ViewHolder, position: Int) {
-        val edition  = editionLists.results!![position]
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val edition = editionLists.results!![position]
         holder.title.text = edition.englishName
+
+        holder.itemView.setOnClickListener {
+            val mDialogueView = LayoutInflater.from(context).inflate(R.layout.d_quran_edition_details, null)
+            val mBuilder = AlertDialog.Builder(context).setView(mDialogueView)
+            val mAlertDualogue = mBuilder.show()
+            mAlertDualogue.quran_edition_details_englishName.text = edition.englishName
+            mAlertDualogue.quran_edition_details_name.text = edition.name
+            mAlertDualogue.quran_edition_details_format.text = edition.format
+            mAlertDualogue.quran_edition_details_type.text = edition.type
+            mAlertDualogue.quran_edition_details_language.text = edition.language
+        }
     }
 
-    inner class ViewHolder(@NonNull itemView: View): RecyclerView.ViewHolder(itemView){
+    inner class ViewHolder(@NonNull itemView: View) : RecyclerView.ViewHolder(itemView) {
         val title: TextView = itemView.findViewById(R.id.quran_edition_name)
         val container: LinearLayout = itemView.findViewById(R.id.quran_edition_container)
 
